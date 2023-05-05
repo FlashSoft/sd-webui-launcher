f9f129443d18759a842e40a6e81ee52421439df6
FlashSoft <babyworld@gmail.com>
2023-05-05 18:48:13 +0800

    补充几个扩展

--- extensions.bat
+++ extensions.bat
@@ -1,4 +1,9 @@
+@echo off
+title SD-WebUI Launcher
+set PATH=%PATH%;%~dp0%git\cmd
 cd stable-diffusion-webui\extensions
 git clone https://github.com/hanamizuki-ai/stable-diffusion-webui-localization-zh_Hans.git
 git clone https://github.com/pkuliyi2015/multidiffusion-upscaler-for-automatic1111.git
 git clone https://github.com/butaixianran/Stable-Diffusion-Webui-Civitai-Helper.git
+git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui-rembg.git
+git clone https://github.com/AlUlkesh/stable-diffusion-webui-images-browser
